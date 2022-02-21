f6d346eae88b11e750d1e14df49d318f6066d516
Johannes Weber <johannes@drweber.de>
2022-02-21 15:33:11 -0800

    add and import InterviewerListItem.scss

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,10 +1,11 @@
 import React from "react";
 import classNames from "classnames";
+import "components/InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
   const classes = classNames("interviewers__item");
   return (
-    <li className={classes}>
+    <li className={classes} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
