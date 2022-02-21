c5d37d6011a6a3ce8a93b5d56bb5f8260e831f81
Johannes Weber <johannes@drweber.de>
2022-02-21 12:47:42 -0800

    fix conditional text

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,10 +7,16 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected,
     "day-list__item--full": !props.spots,
   });
-  return (
+  let spotsText = `${props.spots} spots`;
+  if (props.spots === 1) {
+    spotsText = `1 spot`;
+  } else if (props.spots === 0) {
+    spotsText = `no spots`;
+  }
+  return (    
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{spotsText} remaining</h3>
     </li>
   );
 }
