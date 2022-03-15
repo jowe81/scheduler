21b290e2da6591bf13075a4976b651dd1cb7e9df
Johannes Weber <johannes@drweber.de>
2022-03-15 16:32:07 -0700

    type-check props.interviewers with prop-types

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,8 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 import "components/InterviewerList.scss";
 import InterviewerListItem from "components/InterviewerListItem";
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
   const listItems = props.interviewers.map(interviewer => {
     const data = {
       key: interviewer.id,
@@ -21,3 +23,9 @@ export default function InterviewerList(props) {
     </section>    
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
