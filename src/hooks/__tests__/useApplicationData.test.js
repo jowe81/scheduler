13e75f5bce53fb1096c2be8a7bd290ec504c7e33
Johannes Weber <johannes@drweber.de>
2022-03-18 11:03:23 -0700

    figure out act() warning?

--- src/hooks/__tests__/useApplicationData.test.js
+++ src/hooks/__tests__/useApplicationData.test.js
@@ -1,15 +1,16 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react-hooks";
 
 import useApplicationData from "hooks/useApplicationData";
 
 test("useApplicationData should return state object", () => {
-  const { result } = renderHook(() => useApplicationData());
-  
+  const { result } = renderHook(() => useApplicationData()); //Throws act() warning!
+
   //Very simplistic testing - simply assert that items are returned
   expect(typeof result.current.state).toBe('object');
   expect(typeof result.current.setDay).toBe('function');
   expect(typeof result.current.bookInterview).toBe('function');
   expect(typeof result.current.cancelInterview).toBe('function');
+  
 
   /* Why does the below not work?
   expect(result.current.state).toBe(
